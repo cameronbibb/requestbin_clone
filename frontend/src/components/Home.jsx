849296c2fa04a5f2574302271b7ba10a31161e06
CameronBibb <cameron.bibb@icloud.com>
2026-01-30 23:03:56 -0500

    refactor: clean up and organize CSS styles, removing unused code and improving structure

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,25 +1,24 @@
-import { useNavigate } from 'react-router-dom'
-import helpers from "../services"
+import { useNavigate } from "react-router-dom";
+import helpers from "../services";
 
 const Home = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const newBinHandler = async () => {
     const binPath = await helpers.createBin();
     navigate(`/display/${binPath}`);
-  }
-  
+  };
+
   return (
     <>
-      <h1>Request Bin</h1>
-      <div className='home-page'>
-
-        <button className='copy_style' onClick={newBinHandler}>
+      <div className="home-page">
+        <h1>Request Bin</h1>
+        <button className="copy_style" onClick={newBinHandler}>
           Create a bin
         </button>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
