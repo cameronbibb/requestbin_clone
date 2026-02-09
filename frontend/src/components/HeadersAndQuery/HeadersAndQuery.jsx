1432cab9f8cb9437e2b760d0c062597f47c56413
CameronBibb <cameron.bibb@icloud.com>
2026-02-09 16:20:23 -0500

    refactor: implement HeadersAndQuery component with toggle functionality and styling

--- frontend/src/components/HeadersAndQuery/HeadersAndQuery.jsx
+++ frontend/src/components/HeadersAndQuery/HeadersAndQuery.jsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
+import "./HeadersAndQuery.css";
+
 const HeadersAndQuery = ({ obj, type }) => {
   const array = Object.entries(obj);
 
+  const [displayHeaders, setDisplayHeaders] = useState(false);
+
+  const toggleHeaders = () => {
+    setDisplayHeaders((prev) => !prev);
+  };
+
   return (
-    <>
-      <b>{type}</b>
-      {array.map((nestedArr, idx) => {
-        return (
-          <p key={idx}>
-            {" "}
-            {nestedArr[0]}: {nestedArr[1]}
-          </p>
-        );
-      })}
-    </>
+    <div className="headers-and-query-container">
+      <div className="headers-title">{type}</div>
+      <div className="headers-headers">
+        <div className="headers-toggle" onClick={toggleHeaders}>
+          <span>{displayHeaders ? "▼" : "▶"}</span>({array.length}) headers
+        </div>
+        {displayHeaders && (
+          <div className="headers-list">
+            {array.map((nestedArr, idx) => {
+              return (
+                <div key={idx} className="header-row">
+                  <div className="header-header">{nestedArr[0]}:</div>
+                  <div className="header-value">{nestedArr[1]}</div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
