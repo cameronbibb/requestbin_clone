b5c0f2a58f419fa5f7721ea0208af0c61c0a1dea
CameronBibb <cameron.bibb@icloud.com>
2026-02-21 11:56:29 -0500

    feat: add blurred overlay for viewports below 900px with message to use a larger screen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Bin from "./components/Bin/Bin";
-
 import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/display/:bin_path" element={<Bin />}></Route>
-    </Routes>
+    <>
+      <div className="unsupported-viewport">Please use a larger screen.</div>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/display/:bin_path" element={<Bin />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
